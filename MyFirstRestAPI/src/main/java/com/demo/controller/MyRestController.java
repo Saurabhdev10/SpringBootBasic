9e40e5013cf013289c81c0995f92cbf17abb443c
Saurabh Gupta <126614712+Saurabhdev10@users.noreply.github.com>
2024-03-04 02:06:32 +0530

    pathvariable demo

--- MyFirstRestAPI/src/main/java/com/demo/controller/MyRestController.java
+++ MyFirstRestAPI/src/main/java/com/demo/controller/MyRestController.java
@@ -2,6 +2,7 @@ package com.demo.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,14 @@ public class MyRestController {
 	@GetMapping
 	public Player getData() {
 		return new Player(7, "MS Dhoni", "CSK");
+	}
+	@GetMapping("/{name}")
+	public Player getData1(@PathVariable String name) {
+		return new Player(18,name, "RCB");
+	}
+	@GetMapping("/{name}/{team}")
+	public Player getData2(@PathVariable String name,@PathVariable String team) {
+		return new Player(18,name,team);
 	}
 	@PutMapping
 	public String putData() {
